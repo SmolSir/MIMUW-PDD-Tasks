89b2b06f1a8b2593e80ce0e0b9d825274ee57dc9
SmolSir <b.smolarczyk@student.uw.edu.pl>
2024-05-13 19:01:10 +0200

    use gdown for setup

--- task-1/setup.sh
+++ task-1/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-URL="https://drive.google.com/file/d/1lv4aDS8LrR8LnlKhtaf-bBOnjmaPIHjg/view?usp=drive_link"
+ZIP_ID="1lv4aDS8LrR8LnlKhtaf-bBOnjmaPIHjg"
 ZIP_NAME="data.zip"
 DATA_DIR="data"
 
 # download the data zip
-wget "$URL" -O "$ZIP_NAME"
+gdown "$ZIP_ID" -O "$ZIP_NAME"
 
 # unzip
 unzip "$ZIP_NAME" -d "$DATA_DIR"
